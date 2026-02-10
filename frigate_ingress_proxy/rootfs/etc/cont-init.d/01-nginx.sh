d0c27bd9a75bc0a205ea64487af3f32300866861
pipacsba <44190358+pipacsba@users.noreply.github.com>
2026-02-10 06:37:09 +0100

    Add Nginx initialization script for upstream settings

---
+++ frigate_ingress_proxy/rootfs/etc/cont-init.d/01-nginx.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/with-contenv bash
+set -e
+
+UPSTREAM_HOST="$(jq -r '.upstream_host' /data/options.json)"
+UPSTREAM_PORT="$(jq -r '.upstream_port' /data/options.json)"
+UPSTREAM_HTTPS="$(jq -r '.upstream_https' /data/options.json)"
+
+if [ "$UPSTREAM_HTTPS" = "true" ]; then
+  UPSTREAM_SCHEME="https"
+else
+  UPSTREAM_SCHEME="http"
+fi
+
+export UPSTREAM_HOST UPSTREAM_PORT UPSTREAM_SCHEME
+
+echo "[INFO] Frigate upstream: ${UPSTREAM_SCHEME}://${UPSTREAM_HOST}:${UPSTREAM_PORT}"
+
+envsubst '${UPSTREAM_SCHEME} ${UPSTREAM_HOST} ${UPSTREAM_PORT}' \
+  < /etc/nginx/nginx.conf.template \
+  > /etc/nginx/nginx.conf
